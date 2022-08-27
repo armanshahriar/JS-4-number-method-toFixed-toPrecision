8064ee71af1530d8ba07282ec1e10b02a9b46226
Arman Shahariar <111569490+armanshahriar@users.noreply.github.com>
2022-08-27 10:36:08 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,60 @@
+// Function typeof
+
+var num = 10;
+
+console.log(typeof (num))
+
+var Num = "Arman";
+
+console.log(typeof (Num))
+
+var nUm = true;
+
+console.log(typeof (nUm))
+
+// tostring 
+
+var string = 10;
+
+string = toString(string)
+
+console.log(typeof (string))
+
+// parseInt
+
+var string = 10.22;
+
+string = parseInt(string)
+
+console.log(typeof (string))
+
+// parseFloat
+
+var string = 10.22;
+
+string = parseFloat(string)
+
+console.log(typeof (string))
+
+
+
+// toFixed
+
+var string = 10.22345678;
+
+
+console.log(string.toFixed(1))
+
+
+// toPrecision
+
+var string = 10.22345678;
+
+
+console.log(string.toPrecision(3))
+
+// Number
+
+console.log(typeof (Number("109")))
+
+console.log(Number(190))
